91564a73a3022177dfc0f121a6916a46b3776184
jugyo <jugyo.org@gmail.com>
2012-01-23 23:43:16 +0900

    add #login method to login explicitly

--- examples/bookmark.rb
+++ examples/bookmark.rb
@@ -1,5 +1,5 @@
 require 'instapi'
 
 # i = Instapi.new('username', 'password')
-i = Instapi.new(*ARGV)
+i = Instapi.new(*ARGV).login!
 i.add('https://github.com/jugyo')
--- examples/get_unread.rb
+++ examples/get_unread.rb
@@ -1,7 +1,7 @@
 require 'instapi'
 
 # i = Instapi.new('username', 'password')
-i = Instapi.new(*ARGV)
+i = Instapi.new(*ARGV).login!
 i.unread.each do |bookmark|
   p bookmark
 end
